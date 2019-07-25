3da1702287a9ecbff826bcd94e3727b90d283065
j_h <13113017500>
2019-07-25 14:23:35 +0800

    底部button显示

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+/*
+ * flutter_face + 百度人脸识别;
+ */
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -8,17 +11,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'flutter_face'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
 
   final String title;
 
@@ -41,25 +42,47 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+      body: ContentBody(),
+      floatingActionButton: BottomBody(),
+    );
+  }
+
+  /*
+   * 显示主体内容;
+   */
+  Widget ContentBody() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'You have pushed the button this many times:',
+          ),
+          Text(
+            '$_counter',
+            style: Theme.of(context).textTheme.display1,
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
+    );
+  }
+
+  /*
+   * 显示底部2个按钮
+   */
+  Widget BottomBody() {
+    return ButtonBar(alignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+      FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        child: Icon(Icons.camera_alt),
+      ),
+      FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add_circle),
+      )
+    ]);
   }
 }
