3fa370558004a016dfaf4cebd788a89cf5a5170b
jinhuizxc <1004260403@qq.com>
2019-07-25 23:22:58 +0800

    从相机、相册选择照片设置到image上;

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 /*
  * flutter_face + 百度人脸识别;
+ *
+ * https://www.liulongbin.top/
+ *
+ * Flutter开发中常用的快捷键
+ * https://www.jianshu.com/p/b9efacbf6c34
  */
 void main() => runApp(MyApp());
 
@@ -30,59 +37,87 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
+  // 用户通过摄像头或图片库选择的照片, 需要导入io库
+  File _image;
+
+  // 选择照片
+  void choosePic(ImageSource source) async {
+    var image = await ImagePicker.pickImage(source: source);
+
+    // 设置照片   rn: this.setState()   小程序：this.setState{()}
     setState(() {
-      _counter++;
+      _image = image;
     });
+
+    print("选择的照片 = " + image.toString());
+    // I/flutter: 选择的照片 = File: '/storage/emulated/0/DCIM/Camera/IMG_20190725_211557.jpg'
+    // I/flutter: 选择的照片 = File: '/storage/emulated/0/Android/data/com.example.flutter_face/files/Pictures/c4ddf1e3-e6ac-49bf-92e5-a4a32e789fca7414828943492455094.jpg'
+
+    // 调用api获取颜值信息
+    getFaceInfo();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: ContentBody(),
-      floatingActionButton: BottomBody(),
+      appBar: renderAppBar(),
+      body: renderBody(),
+      floatingActionButton: renderFloatActionButton(),
     );
   }
 
   /*
-   * 显示主体内容;
+   * 渲染页面的主体区域
    */
-  Widget ContentBody() {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            'You have pushed the button this many times:',
-          ),
-          Text(
-            '$_counter',
-            style: Theme.of(context).textTheme.display1,
-          ),
-        ],
-      ),
+  Widget renderBody() {
+    if (_image == null) {
+      // 并非居中，添加center使居中
+      return Center(
+        child: Text('请选择照片!'),
+      );
+    }
+    return Image.file(
+      _image,
+      fit: BoxFit.cover,
+      width: double.infinity,
+      height: double.infinity,
     );
   }
 
   /*
-   * 显示底部2个按钮
+   * 渲染底部的浮动按钮区域
    */
-  Widget BottomBody() {
-    return ButtonBar(alignment: MainAxisAlignment.spaceAround,
+  Widget renderFloatActionButton() {
+    return ButtonBar(
+        alignment: MainAxisAlignment.spaceAround,
         children: <Widget>[
-      FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.camera_alt),
-      ),
-      FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add_circle),
-      )
-    ]);
+          FloatingActionButton(
+            onPressed: () {
+              // 打开相机
+              choosePic(ImageSource.camera);
+            },
+            tooltip: 'Increment',
+            child: Icon(Icons.camera_alt),
+          ),
+          FloatingActionButton(
+            // 打开相册
+            onPressed: () {
+              choosePic(ImageSource.gallery);
+            },
+            tooltip: 'Increment',
+            child: Icon(Icons.photo_library),
+          )
+        ]);
   }
+
+  /*
+   * 渲染头部的appbar
+   */
+  Widget renderAppBar() {
+    return AppBar(
+      title: Text(widget.title),
+    );
+  }
+
+  void getFaceInfo() {}
 }
